e9a9357390683ac553f5c0f8cf3b97ea2705b63b
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-04-15 13:27:59 +0300

    Convert boolean values to strings

---
+++ ConvertBooleanValuesToStrings.sql
@@ -0,0 +1,2 @@
+-- # write your SQL statement here: you are given a table 'booltoword' with column 'bool', return a table with column 'bool' and your result in a column named 'res'.
+SELECT bool, CASE WHEN bool = TRUE THEN 'Yes' WHEN bool = FALSE THEN 'No' END AS res FROM booltoword
