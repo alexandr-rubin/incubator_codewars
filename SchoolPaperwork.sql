7fa8141aeee219cce6fbea226c4a8fa31f6cc8d4
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-04-15 15:17:33 +0300

    School Paperwork

---
+++ SchoolPaperwork.sql
@@ -0,0 +1,2 @@
+-- # write your SQL statement here: you are given a table 'paperwork' with columns 'id', n' and 'm', return a table with columns 'n', 'm' and your result in a column named 'res'.
+SELECT n, m, case when  n < 0 then 0 when m < 0 then 0 else n * m end as res from paperwork
